2bdc23b743763ee64feba2231c8a6f27a91034ee
sim <sim@cheolhwan.com>
2024-08-01 15:45:02 +0900

    스타일시트 업데이트

--- src/theme.ts
+++ src/theme.ts
@@ -1,20 +1,23 @@
-import { ActionIcon, MantineThemeOverride, Menu, Modal, Table, Text } from "@mantine/core"
+import { ActionIcon, HoverCard, MantineThemeOverride, Menu, Modal, Table, Text } from "@mantine/core"
 
 const theme: MantineThemeOverride = {
   defaultRadius: "xs",
+  cursorType: "pointer",
   components: {
     Table: Table.extend({
       defaultProps: {
         highlightOnHover: true,
+        highlightOnHoverColor: "dark.8",
         withTableBorder: true,
         captionSide: "top",
-        style: { fontFeatureSettings: `"tnum"` },
+        style: { whiteSpace: "nowrap", fontFeatureSettings: `"tnum"` },
       },
     }),
     Modal: Modal.extend({ defaultProps: { transitionProps: { duration: 0 } } }),
     Menu: Menu.extend({ defaultProps: { transitionProps: { duration: 0 } } }),
     Text: Text.extend({ defaultProps: { inherit: true } }),
     ActionIcon: ActionIcon.extend({ defaultProps: { variant: "subtle" } }),
+    HoverCard: HoverCard.extend({ defaultProps: { withArrow: true } }),
   },
 }
 
